bdd91d5b45bdc5e294f30daf283bc3aefe3aceee
Kevin Greene <kevin.greene@creditkarma.com>
2018-04-27 09:16:38 -0700

    chore: Cleanup logging logic

---
+++ src/main/logger.ts
@@ -0,0 +1,30 @@
+function isDebug(): boolean {
+    return (
+        process.env.VAULT_DEBUG === 'true' ||
+        process.env.DEBUG === 'true'
+    )
+}
+
+export const log = (msg: string, data?: any) => {
+    if (data !== undefined && isDebug()) {
+        console.log(`[vault-client:info]: ${msg}: `, data)
+    } else if (isDebug()) {
+        console.log(`[vault-client:info]: ${msg}`)
+    }
+}
+
+export const warn = (msg: string, data?: any) => {
+    if (data !== undefined && isDebug()) {
+        console.warn(`[vault-client:warn]: ${msg}: `, data)
+    } else if (isDebug()) {
+        console.warn(`[vault-client:warn]: ${msg}`)
+    }
+}
+
+export const error = (msg: string, data?: any) => {
+    if (data !== undefined) {
+        console.error(`[vault-client:error]: ${msg}: `, data)
+    } else {
+        console.error(`[vault-client:error]: ${msg}`)
+    }
+}
