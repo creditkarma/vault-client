creditkarma/vault-client

commit b40fc070a618c19d602ab317010ddeb07eef735c

Replace `request` with `got` and upgrade Node/Typescript/Hapi  (#17)
Replacing the request (deprecated) library with got as well as updating Node/Typescript/Hapi to their latest supported versions.

src/main/VaultService.ts
@@ -1,5 +1,7 @@
-import { CoreOptions, OptionsWithUri, RequestResponse } from 'request'
-import * as rpn from 'request-promise-native'
+import { Response, OptionsOfJSONResponseBody } from 'got'
+
+import got from 'got'
+
 import * as logger from './logger'
 
 import {
@@ -21,16 +23,11 @@ import * as utils from './utils'
 
 import { loadSystemCerts } from './certs'
 
-const request = rpn.defaults({
-    json: true,
-    resolveWithFullResponse: true,
-    simple: false,
-})
-
-function responseAsError(res: RequestResponse): HVFail {
+function responseAsError(res: Response): HVFail {
     let message: string
-    if (res.body && res.body.errors && res.body.errors.length > 0) {
-        message = res.body.errors[0]
+    const responseBody = res.body as any
+    if (responseBody?.errors?.length > 0) {
+        message = responseBody.errors[0]
     } else {
         message = `Status ${res.statusCode}`
     }
@@ -38,8 +35,11 @@ function responseAsError(res: RequestResponse): HVFail {
     return new HVFail(message)
 }
 
-function fetch(options: OptionsWithUri, token?: string): Promise<any> {
-    const requestOptions: OptionsWithUri =
+function fetch(
+    options: OptionsOfJSONResponseBody,
+    token?: string,
+): Promise<any> {
+    const requestOptions: OptionsOfJSONResponseBody =
         token !== undefined
             ? utils.deepMerge(options, {
                   headers: {
@@ -48,17 +48,17 @@ function fetch(options: OptionsWithUri, token?: string): Promise<any> {
               })
             : options
 
-    return request(requestOptions).then(
-        (res: RequestResponse) => {
+    return got(requestOptions).then(
+        (res: Response) => {
             switch (res.statusCode) {
                 case 200:
                 case 204:
                     return Promise.resolve(res.body)
 
                 case 404:
-                    logger.error(`Resource not found[${requestOptions.uri}]`)
+                    logger.error(`Resource not found[${requestOptions.url}]`)
                     return Promise.reject(
-                        new HVMissingResource(requestOptions.uri),
+                        new HVMissingResource(requestOptions.url),
                     )
 
                 default:
@@ -67,10 +67,10 @@ function fetch(options: OptionsWithUri, token?: string): Promise<any> {
             }
         },
         (err: any) => {
-            logger.error(`Unable to connect[${requestOptions.uri}]`)
+            logger.error(`Unable to connect[${requestOptions.url}]`)
             return Promise.reject(
                 new HVFail(
-                    `Unable to connect[${requestOptions.uri}]. ${err.message}`,
+                    `Unable to connect[${requestOptions.url}]. ${err.message}`,
                 ),
             )
         },
@@ -81,11 +81,11 @@ export interface IVaultServiceArgs {
     destination: string
     protocol?: HttpProtocol
     apiVersion?: 'v1'
-    requestOptions?: CoreOptions
+    requestOptions?: OptionsOfJSONResponseBody
 }
 
 export class VaultService {
-    private defaultOptions: CoreOptions
+    private defaultOptions: OptionsOfJSONResponseBody
     private dest: string
 
     constructor({
@@ -94,11 +94,16 @@ export class VaultService {
         apiVersion = 'v1',
         requestOptions = {},
     }: IVaultServiceArgs) {
-        this.defaultOptions = requestOptions
+        this.defaultOptions = {
+            responseType: 'json',
+            ...requestOptions,
+        }
         if (protocol === 'https') {
             this.defaultOptions = utils.deepMerge(
                 {
-                    ca: loadSystemCerts(),
+                    https: {
+                        certificateAuthority: loadSystemCerts(),
+                    },
                 },
                 this.defaultOptions,
             )
@@ -108,21 +113,23 @@ export class VaultService {
 
     public health(
         token: string,
-        options: CoreOptions = {},
+        options: OptionsOfJSONResponseBody = {},
     ): Promise<IHealthStatusResult> {
         return fetch(
             utils.deepMerge(this.defaultOptions, options, {
-                uri: `${this.dest}/sys/health`,
+                url: `${this.dest}/sys/health`,
                 method: 'GET',
             }),
             token,
         )
     }
 
-    public status(options: CoreOptions = {}): Promise<IStatusResult> {
+    public status(
+        options: OptionsOfJSONResponseBody = {},
+    ): Promise<IStatusResult> {
         return fetch(
             utils.deepMerge(this.defaultOptions, options, {
-                uri: `${this.dest}/sys/init`,
+                url: `${this.dest}/sys/init`,
                 method: 'GET',
             }),
         )
@@ -130,30 +137,35 @@ export class VaultService {
 
     public init(
         data: IInitArgs,
-        options: CoreOptions = {},
+        options: OptionsOfJSONResponseBody = {},
     ): Promise<IInitResult> {
         return fetch(
             utils.deepMerge(this.defaultOptions, options, {
-                uri: `${this.dest}/sys/init`,
+                url: `${this.dest}/sys/init`,
                 json: data,
                 method: 'PUT',
             }),
         )
     }
 
-    public sealStatus(options: CoreOptions = {}): Promise<ISealStatusResult> {
+    public sealStatus(
+        options: OptionsOfJSONResponseBody = {},
+    ): Promise<ISealStatusResult> {
         return fetch(
             utils.deepMerge(this.defaultOptions, options, {
-                uri: `${this.dest}/sys/seal-status`,
+                url: `${this.dest}/sys/seal-status`,
                 method: 'GET',
             }),
         )
     }
 
-    public seal(token: string, options: CoreOptions = {}): Promise<void> {
+    public seal(
+        token: string,
+        options: OptionsOfJSONResponseBody = {},
+    ): Promise<void> {
         return fetch(
             utils.deepMerge(this.defaultOptions, options, {
-                uri: `${this.dest}/sys/seal`,
+                url: `${this.dest}/sys/seal`,
                 method: 'PUT',
             }),
             token,
@@ -162,11 +174,11 @@ export class VaultService {
 
     public unseal(
         data: IUnsealArgs,
-        options: CoreOptions = {},
+        options: OptionsOfJSONResponseBody = {},
     ): Promise<IUnsealResult> {
         return fetch(
             utils.deepMerge(this.defaultOptions, options, {
-                uri: `${this.dest}/sys/unseal`,
+                url: `${this.dest}/sys/unseal`,
                 json: data,
                 method: 'PUT',
             }),
@@ -176,11 +188,11 @@ export class VaultService {
     public read(
         path: string,
         token: string,
-        options: CoreOptions = {},
+        options: OptionsOfJSONResponseBody = {},
     ): Promise<IReadResult> {
         return fetch(
             utils.deepMerge(this.defaultOptions, options, {
-                uri: `${this.dest}/${path}`,
+                url: `${this.dest}/${path}`,
                 method: 'GET',
             }),
             token,
@@ -189,11 +201,11 @@ export class VaultService {
 
     public list(
         token: string,
-        options: CoreOptions = {},
+        options: OptionsOfJSONResponseBody = {},
     ): Promise<IListResult> {
         return fetch(
             utils.deepMerge(this.defaultOptions, options, {
-                uri: `${this.dest}/secret?list=true`,
+                url: `${this.dest}/secret?list=true`,
                 method: 'GET',
             }),
             token,
@@ -204,11 +216,11 @@ export class VaultService {
         path: string,
         data: any,
         token: string,
-        options: CoreOptions = {},
+        options: OptionsOfJSONResponseBody = {},
     ): Promise<void> {
         return fetch(
             utils.deepMerge(this.defaultOptions, options, {
-                uri: `${this.dest}/${path}`,
+                url: `${this.dest}/${path}`,
                 json: data,
                 method: 'POST',
             }),

src/tests/integration/VaultClient.spec.ts
@@ -34,7 +34,7 @@ describe('VaultClient', () => {
 
     // Client expects the token to be available in the local file system
     before(async () => {
-        return new Promise((resolve, reject) => {
+        return new Promise<void>((resolve, reject) => {
             fs.writeFile(mockConfig.tokenPath, token, (err: any) => {
                 resolve()
             })
@@ -42,7 +42,7 @@ describe('VaultClient', () => {
     })
 
     after(async () => {
-        return new Promise((resolve, reject) => {
+        return new Promise<void>((resolve, reject) => {
             fs.unlink(mockConfig.tokenPath, (err) => {
                 resolve()
             })

src/tests/integration/VaultService.spec.ts
@@ -26,7 +26,7 @@ describe('VaultService', () => {
         execSync('curl localhost:8201/client-token').toString(),
     )
 
-    describe('heealth', () => {
+    describe('health', () => {
         it('should read the health status', async () => {
             return service.health(token).then((res) => {
                 expect(res.initialized).to.be.equal(true)
@@ -88,7 +88,10 @@ describe('VaultService', () => {
         const httpsService: any = new VaultService(httpsConfig)
 
         it('should have multiple ca', () => {
-            expect(httpsService.defaultOptions.ca.length).to.be.greaterThan(1)
+            expect(
+                httpsService.defaultOptions.https.certificateAuthority.length >
+                    1,
+            )
         })
     })
 })

.circleci/config.yml
@@ -2,16 +2,16 @@ defaults: &defaults
   working_directory: ~/vault-client
 
 helpers:
-  install_node_8: &install_node_8
+  install_node_16: &install_node_16
     run:
-      name: Install Node 8
+      name: Install Node 16
       command: |
         set +e
         curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
         export NVM_DIR="/opt/circleci/.nvm"
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-        nvm install 8
-        nvm alias default 8
+        nvm install 16
+        nvm alias default 16
         # Each step uses the same `$BASH_ENV`, so need to modify it
         echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
         echo "[ -s \"$NVM_DIR/nvm.sh\" ] && . \"$NVM_DIR/nvm.sh\"" >> $BASH_ENV
@@ -23,7 +23,7 @@ jobs:
     <<: *defaults
     steps:
       - checkout
-      - *install_node_8
+      - *install_node_16
       - run:
           name: Install NPM Dependencies
           command: npm install
@@ -39,7 +39,7 @@ jobs:
     <<: *defaults
     steps:
       - checkout
-      - *install_node_8
+      - *install_node_16
       - run:
           name: Generate .npmrc File
           command: 'echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc'
@@ -57,7 +57,7 @@ jobs:
     <<: *defaults
     steps:
       - checkout
-      - *install_node_8
+      - *install_node_16
       - run:
           name: Generate .npmrc File
           command: 'echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc'

.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     extends: [
+        'prettier',
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
     parserOptions: {
@@ -10,11 +10,10 @@ module.exports = {
         sourceType: 'module', // Allows for the use of imports
     },
     rules: {
-        '@typescript-eslint/array-type': ['error', 'generic'],
+        '@typescript-eslint/array-type': ['error', { default: 'generic'}],
         '@typescript-eslint/no-unused-vars': 0,
         '@typescript-eslint/no-explicit-any': 0,
         '@typescript-eslint/explicit-function-return-type': 0,
-        '@typescript-eslint/interface-name-prefix': ['error', 'always'],
         '@typescript-eslint/camelcase': 0,
         '@typescript-eslint/no-non-null-assertion': 0,
         '@typescript-eslint/no-var-requires': 0,

src/main/VaultClient.ts
@@ -1,4 +1,5 @@
-import { CoreOptions } from 'request'
+import { OptionsOfJSONResponseBody } from 'got'
+
 import { getToken } from './discovery'
 import { HVFail, HVInvalidResponse } from './errors'
 import * as logger from './logger'
@@ -15,7 +16,7 @@ export interface IVaultClientArgs {
     apiVersion?: 'v1'
     protocol?: HttpProtocol
     destination?: string
-    requestOptions?: CoreOptions
+    requestOptions?: OptionsOfJSONResponseBody
     mount?: string
     namespace?: string
     tokenPath?: string
@@ -55,7 +56,10 @@ export class VaultClient {
         })
     }
 
-    public get<T>(key: string, options: CoreOptions = {}): Promise<T> {
+    public get<T>(
+        key: string,
+        options: OptionsOfJSONResponseBody = {},
+    ): Promise<T> {
         return this.getToken().then((tokenValue: string) => {
             const secretPath: string = utils.resolveSecretPath(
                 this.mount,
@@ -91,7 +95,7 @@ export class VaultClient {
     public set<T>(
         key: string,
         value: T,
-        options: CoreOptions = {},
+        options: OptionsOfJSONResponseBody = {},
     ): Promise<void> {
         return this.getToken().then((tokenValue: string) => {
             const secret: string = utils.resolveSecretPath(

src/main/errors.ts
@@ -7,7 +7,7 @@ export class HVInvalidResponse extends Error {
 }
 
 export class HVMissingResource extends Error {
-    constructor(location: string | url.Url) {
+    constructor(location: string | url.URL | undefined) {
         super(`Unable to locate vault resource[${location}]`)
     }
 }

src/main/types.ts
@@ -1,4 +1,4 @@
-import { CoreOptions } from 'request'
+import { OptionsOfJSONResponseBody } from 'got'
 
 export type HttpProtocol = 'http' | 'https'
 
@@ -6,14 +6,14 @@ export interface IServiceConfig {
     apiVersion: 'v1'
     protocol: HttpProtocol
     destination: string
-    requestOptions: CoreOptions
+    requestOptions: OptionsOfJSONResponseBody
 }
 
 export interface IHVConfig {
     apiVersion: 'v1'
     protocol: HttpProtocol
     destination: string
-    requestOptions: CoreOptions
+    requestOptions: OptionsOfJSONResponseBody
     mount: string
     namespace: string
     tokenPath: string

commit aeb2f22ba761718317b590ae030a79069073587f

fix: Throw HVMissingResource when trying to read a missing value from HVault
This appears to be a regression from #17 (b40fc070). Got throws an error by default for non-successful http status codes. This circumvented our code throws HVMissingResource errors on 404s.

src/main/VaultService.ts
@@ -96,6 +96,7 @@ export class VaultService {
     }: IVaultServiceArgs) {
         this.defaultOptions = {
             responseType: 'json',
+            throwHttpErrors: false,
             ...requestOptions,
         }
         if (protocol === 'https') {

src/main/logger.ts
@@ -4,7 +4,7 @@ function isDebug(): boolean {
 
 export const log = (msg: string, data?: any) => {
     if (data !== undefined && isDebug()) {
-        console.log(`[vault-client:info]: ${msg}: `, data)
+        console.log(`[vault-client:info]: ${msg}: ${data}`)
     } else if (isDebug()) {
         console.log(`[vault-client:info]: ${msg}`)
     }
@@ -12,7 +12,7 @@ export const log = (msg: string, data?: any) => {
 
 export const warn = (msg: string, data?: any) => {
     if (data !== undefined && isDebug()) {
-        console.warn(`[vault-client:warn]: ${msg}: `, data)
+        console.warn(`[vault-client:warn]: ${msg}: ${data}`)
     } else if (isDebug()) {
         console.warn(`[vault-client:warn]: ${msg}`)
     }
@@ -20,7 +20,7 @@ export const warn = (msg: string, data?: any) => {
 
 export const error = (msg: string, data?: any) => {
     if (data !== undefined) {
-        console.error(`[vault-client:error]: ${msg}: `, data)
+        console.error(`[vault-client:error]: ${msg}: ${data}`)
     } else {
         console.error(`[vault-client:error]: ${msg}`)
     }

src/tests/integration/VaultClient.spec.ts
@@ -1,9 +1,10 @@
-import { expect } from '@hapi/code'
+import { expect, fail } from '@hapi/code'
 import * as Lab from '@hapi/lab'
 import * as fs from 'fs'
 import { VaultClient } from '../../main/VaultClient'
 import { IHVConfig } from '../../main/types'
 import { clientToken } from './bootstrap'
+import { HVMissingResource } from '../../main/errors'
 
 export const lab = Lab.script()
 
@@ -86,6 +87,21 @@ describe('VaultClient', () => {
             return client.get('obj').then((res: any) => {
                 expect(res).to.equal(mockObj)
             })
+        })
+
+        it('should throw HVMissingResource error when reading a non-existing value from hvault', async () => {
+            return client.get('missing').then(
+                () => {
+                    fail('should have thrown HVMissingResource')
+                },
+                (err: any) => {
+                    expect(err).to.be.equal(
+                        new HVMissingResource(
+                            'http://localhost:8200/v1/secret/missing',
+                        ),
+                    )
+                },
+            )
         })
     })
 

src/tests/integration/VaultService.spec.ts
@@ -1,8 +1,9 @@
-import { expect } from '@hapi/code'
+import { expect, fail } from '@hapi/code'
 import * as Lab from '@hapi/lab'
 import { VaultService } from '../../main/VaultService'
 import { IHVConfig } from '../../main/types'
 import { clientToken } from './bootstrap'
+import { HVMissingResource } from '../../main/errors'
 
 export const lab = Lab.script()
 
@@ -74,6 +75,23 @@ describe('VaultService', () => {
             return service.read('secret/mock', clientToken).then((res: any) => {
                 expect(res.data).to.equal(mockObj)
             })
+        })
+    })
+
+    describe('read missing', () => {
+        it('should throw when reading a missing object from hvault', async () => {
+            return service.read('secret/missing', clientToken).then(
+                () => {
+                    fail('should have thrown HVMissingResource')
+                },
+                (err: any) => {
+                    expect(err).to.be.equal(
+                        new HVMissingResource(
+                            'http://localhost:8200/v1/secret/missing',
+                        ),
+                    )
+                },
+            )
         })
     })
 